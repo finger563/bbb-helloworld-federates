cb1037ec15d124c34f47fdc6f99d0e07b8e84456
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-02-02 08:04:57 -0600

    updated code.

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,6 @@
 package BBBHelloWorld;
 
+import c2w.hla.InteractionRoot;
 
 public class Controller extends ControllerBase {
     
@@ -19,35 +20,59 @@ public class Controller extends ControllerBase {
 
         startAdvanceTimeThread();
 
+        InteractionRoot interactionRoot;
+
         boolean heaterOn = true;
         boolean coolerOn = false;
         boolean fan1On = true;
         boolean fan2On = true;
+	double setPoint = 25; // deg C
+	double tolerance = 1; // deg C
+	
+	double value = setPoint;
+	double diff = 0;
+	String units = "deg C";
 
         while( true ) {
-            ControlMessage msg = create_ControlMessage();
-            msg.set_heater( heaterOn );
-            msg.set_cooler( coolerOn );
-            msg.set_fan1( fan1On );
-            msg.set_fan2( fan2On );
 
             currentTime += 1;
             atr.requestSyncStart();
 
-            System.out.println( "Controller: Sending ControlMessage interaction" );
-            msg.sendInteraction( getRTI(), currentTime );
+            while (  ( interactionRoot = getNextInteractionNoWait() ) != null ) {
+		SensorMessage msg = (SensorMessage)interactionRoot;
+		value = msg.get_Value();
+		units = msg.get_Units();
+		diff = value-setPoint;
+
+		if ( Math.abs(diff) > tolerance && diff > 0 ) {
+		    coolerOn = true;
+		    heaterOn = false;
+		    fan1On = true;
+		    fan2On = true;
+		}
+		else if ( Math.abs(diff) > tolerance && diff < 0 ) {
+		    coolerOn = false;
+		    heaterOn = true;
+		    fan1On = false;
+		    fan2On = false;
+		}
+	    }
+
+	    ControlMessage msg = create_ControlMessage();
+	    msg.set_heater( heaterOn );
+	    msg.set_cooler( coolerOn );
+	    msg.set_fan1( fan1On );
+	    msg.set_fan2( fan2On );
+
+	    System.out.println( "Controller: Sending ControlMessage interaction to bring current temp" 
+				+ value + units + " to " + setPoint + "deg C" );
+	    msg.sendInteraction( getRTI(), currentTime );
 
             AdvanceTimeRequest newATR = new AdvanceTimeRequest( currentTime );
             putAdvanceTimeRequest( newATR );
 
             atr.requestSyncEnd();
             atr = newATR;
-
-            // Switch fan settings
-            heaterOn = !heaterOn;
-            coolerOn = !heaterOn;
-            fan1On = !fan1On;
-            fan2On = !fan2On;
         }
 
     }
